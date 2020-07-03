8e02f9ea59ea5888c9b6a4665681924dbd48e465
Fuu <makinoyuki1222@gmail.com>
2020-07-03 17:24:50 +0900

    add all

---
+++ src/custom/extract_ppgmel.py
@@ -0,0 +1,262 @@
+"""Modified from https://github.com/NVIDIA/tacotron2"""
+# modify by common/data_utils.py
+
+import pickle
+import random
+import numpy as np
+import torch
+import torch.utils.data
+
+import sys
+sys.path.insert(0, '/home/fuu/sdb/fac-via-ppg/src')
+
+from common.utils import load_filepaths
+from common.utterance import Utterance
+from common import layers
+from ppg import DependenciesPPG
+from scipy.io import wavfile
+from common import feat
+from common import ppg
+from common.hparams_628 import create_hparams
+
+
+
+
+# First order, dx(t) = 0.5(x(t + 1) - x(t - 1))
+DELTA_WIN = [0, -0.5, 0.0, 0.5, 0]
+# Second order, ddx(t) = 0.5(dx(t + 1) - dx(t - 1)) = 0.25(x(t + 2) - 2x(t)
+# + x(t - 2))
+ACC_WIN = [0.25, 0, -0.5, 0, 0.25]
+
+
+def get_ppg(wav_path, deps):
+    fs, wav = wavfile.read(wav_path)
+    wave_data = feat.read_wav_kaldi_internal(wav, fs)
+    seq = ppg.compute_full_ppg_wrapper(wave_data, deps.nnet, deps.lda, 10)
+    return seq
+
+
+def compute_dynamic_vector(vector, dynamic_win, frame_number):
+    """Modified from https://github.com/CSTR-Edinburgh/merlin/blob/master
+    /srcfrontend/acoustic_base.py
+    Compute dynamic features for a data vector.
+    Args:
+        vector: A T-dim vector.
+        dynamic_win: What type of dynamic features to compute. See DELTA_WIN
+        and ACC_WIN.
+        frame_number: The dimension of 'vector'.
+    Returns:
+        Dynamic feature vector.
+    """
+    vector = np.reshape(vector, (frame_number, 1))
+
+    win_length = len(dynamic_win)
+    win_width = int(win_length / 2)
+    temp_vector = np.zeros((frame_number + 2 * win_width, 1))
+    dynamic_vector = np.zeros((frame_number, 1))
+
+    temp_vector[win_width:frame_number + win_width] = vector
+    for w in range(win_width):
+        temp_vector[w, 0] = vector[0, 0]
+        temp_vector[frame_number + win_width + w, 0] = vector[
+            frame_number - 1, 0]
+
+    for i in range(frame_number):
+        for w in range(win_length):
+            dynamic_vector[i] += temp_vector[i + w, 0] * dynamic_win[w]
+
+    return dynamic_vector
+
+
+def compute_dynamic_matrix(data_matrix, dynamic_win):
+    """Modified from https://github.com/CSTR-Edinburgh/merlin/blob/master
+    /srcfrontend/acoustic_base.py
+    Compute dynamic features for a data matrix. Calls compute_dynamic_vector
+    for each feature dimension.
+    Args:
+        data_matrix: A (T, D) matrix.
+        dynamic_win: What type of dynamic features to compute. See DELTA_WIN
+        and ACC_WIN.
+    Returns:
+        Dynamic feature matrix.
+    """
+    frame_number, dimension = data_matrix.shape
+    dynamic_matrix = np.zeros((frame_number, dimension))
+
+    # Compute dynamic feature dimension by dimension
+    for dim in range(dimension):
+        dynamic_matrix[:, dim:dim + 1] = compute_dynamic_vector(
+            data_matrix[:, dim], dynamic_win, frame_number)
+
+    return dynamic_matrix
+
+
+def compute_delta_acc_feat(matrix, is_delta=False, is_acc=False):
+    """A wrapper to compute both the delta and delta-delta features and
+    append them to the original features.
+    Args:
+        matrix: T*D matrix.
+        is_delta: If set to True, compute delta features.
+        is_acc: If set to True, compute delta-delta features.
+    Returns:
+        matrix: T*D (no dynamic feature) | T*2D (one dynamic feature) | T*3D
+        (two dynamic features) matrix. Original feature matrix concatenated
+    """
+    if not is_delta and is_acc:
+        raise ValueError('To use delta-delta feats you have to also use '
+                         'delta feats.')
+    if is_delta:
+        delta_mat = compute_dynamic_matrix(matrix, DELTA_WIN)
+    if is_acc:
+        acc_mat = compute_dynamic_matrix(matrix, ACC_WIN)
+    if is_delta:
+        matrix = np.concatenate((matrix, delta_mat), axis=1)
+    if is_acc:
+        matrix = np.concatenate((matrix, acc_mat), axis=1)
+    return matrix
+
+
+def append_ppg(feats, f0):
+    """Append log F0 and its delta and acc
+
+    Args:
+        feats:
+        f0:
+
+    Returns:
+
+    """
+    num_feats_frames = feats.shape[0]
+    num_f0_frames = f0.shape[0]
+    final_num_frames = min([num_feats_frames, num_f0_frames])
+    feats = feats[:final_num_frames, :]
+    f0 = f0[:final_num_frames]
+    lf0 = np.log(f0 + np.finfo(float).eps)  # Log F0.
+    lf0 = lf0.reshape(lf0.shape[0], 1)  # Convert to 2-dim matrix.
+    lf0 = compute_delta_acc_feat(lf0, True, True)
+    return np.concatenate((feats, lf0), axis=1)
+
+
+class PPGMelLoader_x(torch.utils.data.Dataset):
+    """Loads [ppg, mel] pairs."""
+
+    def __init__(self, data_utterance_paths, cache_path, hparams, bs, loop):
+        """Data loader for the PPG->Mel task.
+
+        Args:
+            data_utterance_paths: A text file containing a list of file paths.
+            hparams: The hyper-parameters.
+        """
+        self.data_utterance_paths = load_filepaths(data_utterance_paths)
+        self.max_wav_value = 32768.0
+        self.sampling_rate = hparams.sampling_rate
+        self.is_full_ppg = True
+        self.is_append_f0 = False
+        self.is_cache_feats = True
+        self.feats_cache_path = cache_path
+        self.ppg_subsampling_factor = 1
+        self.ppg_deps = DependenciesPPG()
+        # 20 data = n(4) * b(5)
+        self.n = int(bs) - 1
+        self.b = 5
+        self.l = int(loop) - 1
+
+        self.stft = layers.TacotronSTFT(
+            hparams.filter_length, hparams.hop_length, hparams.win_length,
+            hparams.n_acoustic_feat_dims, self.sampling_rate,
+            hparams.mel_fmin, hparams.mel_fmax)
+        random.seed(hparams.seed)
+        random.shuffle(self.data_utterance_paths)
+        
+        if self.n > 0:
+            with open(self.feats_cache_path, 'rb') as f:
+                data = pickle.load(f)
+            self.ppg_sequences = data[0]
+            self.acoustic_sequences = data[1]
+        else:
+            self.ppg_sequences = []
+            self.acoustic_sequences = []
+
+        for utterance_path in self.data_utterance_paths[self.n * self.b + self.l * 20 : (self.n+1) * self.b + self.l * 20]:
+
+            ppg_feat_pair = self.extract_utterance_feats(utterance_path,
+                                                self.is_full_ppg)
+            self.ppg_sequences.append(ppg_feat_pair[0].astype(
+                np.float32))
+            self.acoustic_sequences.append(ppg_feat_pair[1])
+        
+        if self.is_cache_feats:
+            print('Caching data to %s.' % self.feats_cache_path)
+            with open(self.feats_cache_path, 'wb+') as f:
+                pickle.dump([self.ppg_sequences, self.acoustic_sequences], f) 
+
+
+
+    def extract_utterance_feats(self, data_utterance_path, is_full_ppg=False):
+        """Get PPG and Mel (+ optional F0) for an utterance.
+
+        Args:
+            data_utterance_path: The path to the data utterance protocol buffer.
+            is_full_ppg: If True, will use the full PPGs.
+
+        Returns:
+            feat_pairs: A list, each is a [pps, mel] pair.
+        """
+        utt = Utterance()
+        ppg_deps = DependenciesPPG()
+        fs, wav = wavfile.read(data_utterance_path)
+        utt.fs = fs
+        utt.wav = wav
+        utt.ppg = get_ppg(data_utterance_path, ppg_deps)
+
+        audio = torch.FloatTensor(utt.wav.astype(np.float32))
+        fs = utt.fs
+
+        if fs != self.stft.sampling_rate:
+            raise ValueError("{} SR doesn't match target {} SR".format(
+                fs, 22050))
+        audio_norm = audio / self.max_wav_value
+        audio_norm = audio_norm.unsqueeze(0)
+        audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
+        # (1, n_mel_channels, T)
+        acoustic_feats = self.stft.mel_spectrogram(audio_norm)
+        # (n_mel_channels, T)
+        acoustic_feats = torch.squeeze(acoustic_feats, 0)
+        # (T, n_mel_channels)
+        acoustic_feats = acoustic_feats.transpose(0, 1)
+        return [utt.ppg, acoustic_feats]
+
+    def __getitem__(self, index):
+        """Get a new data sample in torch.float32 format.
+
+        Args:
+            index: An int.
+
+        Returns:
+            T*D1 PPG sequence, T*D2 mels
+        """
+        if self.ppg_subsampling_factor == 1:
+            curr_ppg = self.ppg_sequences[index]
+        else:
+            curr_ppg = self.ppg_sequences[index][
+                       0::self.ppg_subsampling_factor, :]
+
+        return torch.from_numpy(curr_ppg), self.acoustic_sequences[index]
+
+    def __len__(self):
+        return len(self.ppg_sequences)
+
+
+if __name__ == '__main__':
+
+    training_files_path = "/home/fuu/sdb/fac-via-ppg/data/filelists/cmu/bdl_val_list.txt"
+    txtname = sys.argv[2]
+
+    # cache_path = "/home/fuu/sdb/data/cmu/ppg_mel_pair/bdl_train/file_20_" + txtname + ".txt"            # file: 45
+    cache_path = "/home/fuu/sdb/data/cmu/ppg_mel_pair/bdl_val/file_20_" + txtname + ".txt"                # file: 11
+    hparams = create_hparams()
+    PPGMelLoader_x(training_files_path, cache_path, hparams, sys.argv[1], sys.argv[2])
+
+    with open(cache_path, 'rb') as f:
+        data = pickle.load(f)
+    print("#### shape of data:", np.shape(data), " ####")
---
+++ src/custom/my_preprocess.py
@@ -0,0 +1,372 @@
+# Library
+import sys
+import os
+import wave
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+import common.data_utils as du
+import ppg
+import pickle
+
+from common.hparams_628 import create_hparams_stage
+from script.train_ppg2mel_new import load_model
+from ppg import DependenciesPPG
+import seaborn as sns
+from scipy.io import wavfile
+from common import feat
+from common.layers import TacotronSTFT
+from waveglow.denoiser import Denoiser
+
+
+"""
+生成音频文件列表txt的相关函数
+read_list()
+segment_dataset()
+save_listtxt()
+"""
+# 读取文件路径到列表
+def read_listdir(path):
+    file_name_list = os.listdir(path)
+    file_path_list = []
+    for fn in file_name_list:
+        fp = os.path.join(path, fn)
+        file_path_list.append(fp)
+    return file_path_list
+
+# 保存路径到txt
+def save_listtxt(file_path_list, save_path):
+    with open(save_path, "w") as f:
+        for fp in file_path_list:
+            f.write(fp)
+            f.write("\n")
+
+# 分割训练集和验证集
+def segment_dataset(train_ratio, file_path_list):
+    train_path_list = file_path_list[:int(len(file_path_list)*train_ratio)]
+    val_path_list = file_path_list[int(len(file_path_list)*train_ratio):]
+    print("Num of Train: %d, Num of Val: %d" % (len(train_path_list), len(val_path_list)))
+    return train_path_list, val_path_list
+
+# 合并pickle数据
+def merge_pickle(file_path):
+    # load list of pickle file path
+    fpl = []
+    for fn in os.listdir(file_path):
+        fp = os.path.join(file_path, fn)
+        fpl.append(fp)
+    # load pickle data in to new list
+    ppg_sequences = []
+    acoustic_sequences = []
+    for fp in fpl:
+        print('Loading data from %s.' % fp)
+        with open(fp, 'rb') as f:
+            data = pickle.load(f)
+            ppg_sequences.extend(data[0])
+            acoustic_sequences.extend(data[1])
+    # save the new pickle data
+    merge_file_name = os.path.join(file_path, "merge.txt") 
+    print('Caching data to %s.' % merge_file_name)
+    with open(merge_file_name, 'wb+') as f:
+        pickle.dump([ppg_sequences, acoustic_sequences], f) 
+
+
+class Audio:
+    """音频对象"""
+    def __init__(self, audio_path=""):
+        """读取path"""
+        self.path = audio_path
+        self.get_audio_from_path()
+
+    def get_audio_from_path(self):
+        """运行获取来自文件的音频数据"""
+        if self.path == "":
+            print("Please call the get_audio_from_data(data) or input a path by calling the get_path(self, path)!")
+        else:
+            f = self.__get_sr__()
+            self.au, self.sr = librosa.load(self.path, sr=f) 
+        
+    def audio_detail(self):
+        '''
+        查看音频属性, sr...
+        print 声道，采样宽度，帧速率，帧数，唯一标识，无损
+        '''
+        print('---------声音信息------------')
+        with wave.open(self.path, 'rb') as f:
+            for item in enumerate(f.getparams()):
+                print(item)
+
+    def plot_waveform(self):
+        """画出波形"""
+        # self.get_audio_from_path()
+        time = self.au.shape[0] / self.sr
+        st = 1 / self.sr
+        x_seq = np.arange(0, time, st)
+
+        plt.plot(x_seq, self.au, 'blue')
+        plt.xlabel("time (s)")
+        plt.show()
+
+    def plot_ppg(self):
+        """画出PPGs"""
+        # deps = ppg.DependenciesPPG()
+        # ppg = du.get_ppg(self.path, deps)
+        pass
+
+    def return_audio_data(self):
+        try:
+            return self.au, self.sr
+        except AttributeError:
+            print("Please load audio data!")
+    
+    def get_path(self, path):
+        """传入path"""
+        self.path = path
+        print("Load audio from the path: ", self.path)
+
+    def play_audio(self):
+        """jupyter用"""
+        ipd.Audio(self.path)
+
+    def get_audio_from_data(self, data, sr):
+        """
+        好像一般不会用到
+        运行获取来自数组的音频数据
+        data: Audio data, type: 1-D nparray
+        sr: Sample rate, type: int
+        """
+        self.au = data
+        self.sr = sr
+
+    def __get_sr__(self):
+        """获得采样率的参数"""
+        file = wave.open(self.path)
+        f = file.getparams().framerate  # 采样频率 
+        return f
+
+
+
+# ------------ PPGs ----------------- #
+# ----------------------------------- #
+# Get PPGs
+def get_ppg(wav_path, deps):
+    """
+    Input:
+        wav_path: *.wav path
+        deps = DependenciesPPG()
+    Library:
+        from scipy.io import wavfile
+        from common import feat
+    """
+    fs, wav = wavfile.read(wav_path)
+    wave_data = feat.read_wav_kaldi_internal(wav, fs)
+    seq = ppg.compute_full_ppg_wrapper(wave_data, deps.nnet, deps.lda, 10)
+    return seq
+
+
+# ------------ P2M ------------------ #
+# ----------------------------------- #
+# data to GPU
+def to_gpu(x):
+    x = x.contiguous()
+
+    if torch.cuda.is_available():
+        x = x.cuda(non_blocking=True)
+    return torch.autograd.Variable(x)
+
+# Predict Function
+def get_inference(seq, model, is_clip=False):
+    """Tacotron inference.
+
+    Args:
+        seq: T*D numpy array.
+        model: Tacotron model.
+        is_clip: Set to True to avoid the artifacts at the end.
+
+    Returns:
+        synthesized mels.
+    """
+    # (T, D) numpy -> (1, D, T) cpu tensor
+    seq = torch.from_numpy(seq).float().transpose(0, 1).unsqueeze(0)
+    # cpu tensor -> gpu tensor
+    seq = to_gpu(seq)
+    mel_outputs, mel_outputs_postnet, _, alignments = model.inference(seq)
+    if is_clip:
+        return mel_outputs_postnet[:, :, 10:(seq.size(2)-10)]
+    else:
+        return mel_outputs_postnet
+
+# Load p2m model for predict the Mel from the PPG
+def load_p2m_model(checkpoint_path):   
+    """
+    Load PPG2Mel pre-train model
+    Library:
+        from common.hparams import create_hparams_stage
+        from script.train_ppg2mel_new import load_model
+    Input:
+        p2m's checkpoint_path
+    """
+    hparams = create_hparams_stage()
+
+    tacotron_model = load_model(hparams)
+    tacotron_model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
+    _ = tacotron_model.eval()
+    return tacotron_model
+
+# Normalization for Mel
+def MaxMinNormalization(X, Max, Min):
+    X_n = (X - Min) / (Max - Min)
+    return X_n
+
+# Preprocess mel data for plot
+def preprocess_mel_for_p2m_plot(path, tacotron_model):
+    '''
+    mel from model
+
+    Library:
+        from ppg import DependenciesPPG
+    '''
+    is_cal = 1
+    if is_cal:
+        deps = DependenciesPPG()
+        teacher_utt_path = path
+        teacher_ppg_cal = get_ppg(teacher_utt_path, deps)
+    else:
+        # 从磁盘读入
+        pass
+    mel_ac_cal = get_inference(teacher_ppg_cal, tacotron_model)
+    mel_ac_cal_S_0 = mel_ac_cal.cpu().detach().numpy()[0]
+    mel_ac_cal_tS_0 = mel_ac_cal_S_0[::-1]
+    
+    model_mel = MaxMinNormalization(mel_ac_cal_tS_0, np.max(mel_ac_cal_tS_0), np.min(mel_ac_cal_tS_0))
+    print("Shape of model mel:", np.shape(model_mel))
+    
+    '''mel from STFT'''
+    mel_au_cal, sr = librosa.load(teacher_utt_path, sr=16000)
+    mel_au_cal_S_1 = librosa.feature.melspectrogram(mel_au_cal, sr=sr, n_fft=1024, hop_length=160, n_mels=80)
+    mel_au_cal_logS_1 = librosa.power_to_db(abs(mel_au_cal_S_1[::-1]))
+    
+    stft_mel = MaxMinNormalization(mel_au_cal_logS_1, np.max(mel_au_cal_logS_1), np.min(mel_au_cal_logS_1))
+    print("Shape of stft mel:", np.shape(stft_mel))
+    
+    return model_mel, stft_mel
+
+# Plot spectrogram with <model_syn_mel, origin_stft_mel>
+def draw_mel_pair(mel_list, path):
+    """
+    plot the Mel fig for model-mel and origin-mel
+    Library:
+        import seaborn as sns
+    """
+
+    fig, ax = plt.subplots(2,1, figsize=(15, 9), sharex=True) 
+    titles = ["Model Mel", "STFT Mel"]
+    for i, mel in enumerate(mel_list):
+        p = sns.heatmap(mel, xticklabels=50, yticklabels=20, cmap='viridis', ax=ax[i]) 
+        p.set_yticklabels([80,60,40,20])
+        ax[i].set_title(titles[i] + " for " + "<" + path.split("data/")[1] + ">") 
+        ax[i].set_ylabel("Channel")
+        ax[i].set_xlabel("Frame")
+
+# ----------------------------------- #
+# Execute process
+def main_for_p2m(checkpoint_path, path):
+    # Load Model
+    tacotron_model = load_p2m_model(checkpoint_path)
+    # Predict model_Mel and Caculate STFT_Mel
+    mel_list = preprocess_mel_for_p2m_plot(path, tacotron_model)
+    # plot mel pair
+    draw_mel_pair(mel_list, path)
+
+class MelSpectrogrom:
+    """
+    载入音频或者mel数据
+    处理相关数据
+    画mel图
+    """
+    pass
+
+
+# ------------ M2W ------------------ #
+# ----------------------------------- #
+# STFT
+
+
+MAX_WAV_VALUE = 32768.0
+waveglow_sigma = 0.6
+denoiser_strength = 0.005
+
+# ------------ Data ----------------- #
+# Loads wavdata into torch array
+def load_wav_to_torch(full_path):
+    """
+    Loads wavdata into torch array
+    """
+    sampling_rate, data = wavfile.read(full_path)
+    return torch.from_numpy(data).float(), sampling_rate
+
+# Get Mel from path for m2w
+def get_mel_for_test(file_path):
+    """
+    Library:
+        from common.layers import TacotronSTFT
+    """
+    audio, sr = load_wav_to_torch(file_path)
+    audio_norm = audio / MAX_WAV_VALUE
+    audio_norm = audio_norm.unsqueeze(0)
+    audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
+    stft = TacotronSTFT(filter_length=1024,
+                                hop_length=160,
+                                win_length=1024,
+                                sampling_rate=sr,
+                                mel_fmin=0.0, mel_fmax=8000.0)
+    melspec = stft.mel_spectrogram(audio_norm)
+    return melspec
+
+# Load mel from model for m2w
+def preprocess_model_mel_for_m2w(path, tacotron_model):
+    '''mel from model'''
+    deps = DependenciesPPG()
+    teacher_utt_path = path
+    teacher_ppg_cal = get_ppg(teacher_utt_path, deps)
+    mel_ac_cal = get_inference(teacher_ppg_cal, tacotron_model)
+    return mel_ac_cal
+
+# Load model
+def load_waveglow_model(path):
+    model = torch.load(path)['model']
+    model = model.remove_weightnorm(model)
+    model.cuda().eval()
+    return model
+
+# Mel to Wave
+def waveglow_audio(mel, waveglow, sigma, is_cuda_output=False):
+    mel = torch.autograd.Variable(mel.cuda())
+    if not is_cuda_output:
+        with torch.no_grad():
+            audio = 32768 * waveglow.infer(mel, sigma=sigma)[0]
+        audio = audio.cpu().numpy()
+        audio = audio.astype('int16')
+    else:
+        with torch.no_grad():
+            audio = waveglow.infer(mel, sigma=sigma).cuda()
+    return audio
+
+# Denoiser
+def load_denoiser(waveglow_path):
+    """
+    Library:
+        from waveglow.denoiser import Denoiser
+    """
+    waveglow_for_denoiser = torch.load(waveglow_path)['model']
+    waveglow_for_denoiser.cuda()
+    denoiser_mode = 'zeros'
+    denoiser = Denoiser(waveglow_for_denoiser, mode=denoiser_mode)
+    return denoiser
+
+
+# if __name__ == "__main__":
+
+    
